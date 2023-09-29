b45e0b20292628ebe4eb429a7c64749ac0be1401
adrianag57 <adrianag57@educastur.es>
2023-09-29 14:02:07 +0200

    cambios

---
+++ ejercicio1/js/ejercicio1.js
@@ -0,0 +1,44 @@
+var num = prompt("Introduce un número");
+
+    function esEntero() {
+
+        var result = false;
+
+        if (parseInt(num) == num) {
+
+            result = true;
+            alert ("El número es entero");
+        }
+        else {
+
+            console.error("El número no es entero"); 
+        }
+
+        return result;
+    }
+
+    function esPositivo() {
+
+        var result = false;
+
+        if (parseInt(num) == num && num > 0) {
+
+            alert ("El número es positivo");
+        }
+    }
+
+    function sumatorio() {
+
+        var sumatorio = 0;
+
+        for (var i = 0; i < num; i++) {
+
+            sumatorio += i;
+        }
+
+        return sumatorio;
+    }
+
+esEntero();
+esPositivo();
+console.log(sumatorio());
