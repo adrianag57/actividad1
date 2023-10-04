5c8a3b3f623353e5835f9b91c792716ccf4fbd98
adrianag57 <adrianag57@educastur.es>
2023-10-04 19:02:12 +0200

    cambios

--- ejercicio3/js/ejercicio3.js
+++ ejercicio3/js/ejercicio3.js
@@ -2,12 +2,33 @@ const MAXIMO = 100;
 const MINIMO = 1; 
 const DIVISORES = [2,3,5,10];
 
-var num = prompt("Introduce un número entre 1 y 100");
+//pedimos un numero entero positivo
+var num = prompt("Introduce un número entero positivo entre 0 y 100");
 
-    if(parseInt(num) > MAXIMO && parseInt(num) < MINIMO) {
+    while (isNaN(num) || num > MAXIMO || num < MINIMO) {
 
         //creamos un aviso en la consola de que el número no está entre 0 y 100
         console.log("El número no es entero positivo");
         //ponemos en pantalla que hay un error y debes introducir otro numero
-        num = parseInt(prompt("Error. Introduce un número entero positivo"));
+        num = prompt("Error. Introduce un número entero positivo");
     }
+
+    function esDivisible(num, divisor) {
+
+        if(num % divisor == 0) {
+
+            return true;
+        } else {
+            
+            return false;
+        }
+    }
+
+var division = [];
+
+    for(var i = 0; i < DIVISORES.length; i++) {
+
+        division += ("¿Es " + num + " divisible entre " + DIVISORES[i] + "? " + esDivisible(num, DIVISORES[i]) + "\n");
+    }
+
+window.alert(division);
